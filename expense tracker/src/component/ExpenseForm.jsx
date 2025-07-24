87dc7aedff65785e404f3e93326a99f68aa9e05e
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-24 07:56:25 +0530

    correct some syntax

--- expense tracker/src/component/ExpenseForm.jsx
+++ expense tracker/src/component/ExpenseForm.jsx
@@ -5,21 +5,21 @@ function ExpenseForm() {
    <>
     <main>
       <h1>Track Your Expense</h1>
-      <div class="expense-tracker">
-        <form class="expense-form">
-          <div class="input-container">
-            <label for="title">Title</label>
+      <div className="expense-tracker">
+        <form className="expense-form">
+          <div className="input-container">
+            <label htmlFor="title">Title</label>
             <input id="title" />
           </div>
-          <div class="input-container">
-            <label for="category">Category</label>
+          <div className="input-container">
+            <label htmlFor="category">Category</label>
             <input id="category" />
           </div>
-          <div class="input-container">
-            <label for="amount">Amount</label>
+          <div className="input-container">
+            <label htmlFor="amount">Amount</label>
             <input id="amount" />
           </div>
-          <button class="add-btn">Add</button>
+          <button className="add-btn">Add</button>
         </form>
        
       </div>
