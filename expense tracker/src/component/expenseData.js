1c9b1975d2de3d027408b0572ccfde36e27cd256
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-25 18:27:48 +0530

    Fixes string formatting for expense data entries
    Corrects title and category fields in expense data to use string literals, ensuring consistency and preventing potential errors in data handling.

--- expense tracker/src/component/expenseData.js
+++ expense tracker/src/component/expenseData.js
@@ -1,25 +1,25 @@
 export default [
     {
         id: crypto.randomUUID(),
-        title: milk,
-        category: grocery, 
+        title: "milk",
+        category: "grocery", 
         amount:40
     },
     {
         id: crypto.randomUUID(),
-        title: shirt,
-        category: clothes, 
+        title: "shirt",
+        category: "clothes", 
         amount:40
     },{
         id: crypto.randomUUID(),
-        title: vegitable,
-        category: grocery, 
+        title: "vegitable",
+        category: "grocery", 
         amount:40
     },
 {
         id: crypto.randomUUID(),
-        title: electricitybill,
-        category: Bill, 
+        title: "electricitybill",
+        category: "Bill", 
         amount:40
     },
 
