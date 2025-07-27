d059710443063fc1608cb8740093d67598b7995a
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-27 15:09:35 +0530

    Enhances Select component with dynamic options
    Updates the Select component to accept dynamic options and a default option.
    
    This change improves flexibility by allowing the component to receive an array of options instead of hardcoded values, enhancing reusability across different contexts.

--- expense tracker/src/component/Select.jsx
+++ expense tracker/src/component/Select.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 
-const Select = ({id, label, name, title, value, onChange, error }) => {
-    return(
-        <>
-         <div className="input-container">
-            <label htmlFor={id}>{label}</label>
-            <select id={id}
-             name={name}
-             value={value}
-             onChange = {onChange}
-             >
-                  <option value="" hidden>All</option>
-                  <option value="grocery">Grocery</option>
-                  <option value="clothes">Clothes</option>
-                  <option value="bills">Bills</option>
-                  <option value="education">Education</option>
-                  <option value="medicine">Medicine</option>
-             </select>
-             <p className='err'>{error}</p>
-          </div>
-        </>
-    )
-
-}
-export default Select
+const Select = ({
+  id,
+  label,
+  name,
+  title,
+  value,
+  onChange,
+  options,
+  defaultOption,
+  error,
+}) => {
+  return (
+    <>
+      <div className="input-container">
+        <label htmlFor={id}>{label}</label>
+        <select id={id} name={name} value={value} onChange={onChange}>
+          {defaultOption && (
+            <option value="" hidden>
+              {defaultOption}
+            </option>
+          )}
+          {options.map((option, i) => {
+            <option key={i} value={option}>
+              {option}
+            </option>;
+          })}
+        </select>
+        <p className="err">{error}</p>
+      </div>
+    </>
+  );
+};
+export default Select;
