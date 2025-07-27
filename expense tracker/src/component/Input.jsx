983892509375cd85b1bf5868f0f603e13739cc17
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-27 13:58:22 +0530

    Adds Input component for user input handling
    Implements a new Input component that allows users to enter data with associated labels and error messages.
    
    This enhancement improves user experience by providing clear input fields and error feedback.

---
+++ expense tracker/src/component/Input.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Input({id, label, name, title, value, onChange, error }) {
+  return (
+    <>
+    <div className="input-container">
+            <label htmlFor={id}>{label}</label>
+            <input id={id} name={name} 
+            value={value} 
+            onChange = {onChange} />
+           <p className='err'>{error.title}</p> 
+          </div>
+    </>
+  )
+}
+
+export default Input
