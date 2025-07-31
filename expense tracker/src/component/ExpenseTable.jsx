3a37479bd9a9f8cfb669533b9ebc8f33afbd64b6
vivek prajapati <vivek011prajapati@gmail.com>
2025-07-31 14:00:52 +0530

    fix some error and write logic for sorting

--- expense tracker/src/component/ExpenseTable.jsx
+++ expense tracker/src/component/ExpenseTable.jsx
@@ -36,7 +36,39 @@ function ExpenseTable({ expenses, setexpenses, setExpense, setEditingRowId }) {
       >
         <thead>
           <tr>
-            <th>Title</th>
+            <th className="amount-column">
+              <div>
+                <span>Title</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  viewBox="0 0 384 512"
+                  className="arrow up-arrow"
+                  onClick={() => {
+                    setSortCallback(
+                      () => (a, b) => a.title.localeCompare(b.title)
+                    )
+                  }}
+                >
+                  <title>Ascending</title>
+                  <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="10"
+                  viewBox="0 0 384 512"
+                  className="arrow down-arrow"
+                 onClick={() => {
+                    setSortCallback(
+                      () => (a, b) => a.title.localeCompare(b.title)
+                    )
+                  }}
+                >
+                  <title>Descending</title>
+                  <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
+                </svg>
+              </div>
+            </th>
             <th>
               <select onChange={(e) => setQuery(e.target.value.toLowerCase())}>
                 <option value="">All</option>
@@ -55,8 +87,9 @@ function ExpenseTable({ expenses, setexpenses, setExpense, setEditingRowId }) {
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow up-arrow"
-                  onClick={() => { setSortCallback((a,b) => a.amount - b.amount) }
-                }
+                  onClick={() => {
+                    setSortCallback(() => (a, b) => b.amount - a.amount);
+                  }}
                 >
                   <title>Ascending</title>
                   <path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z" />
@@ -66,8 +99,9 @@ function ExpenseTable({ expenses, setexpenses, setExpense, setEditingRowId }) {
                   width="10"
                   viewBox="0 0 384 512"
                   className="arrow down-arrow"
-                  onClick={() => { setSortCallback((a,b) => b.amount - a.amount) }
-                }
+                  onClick={() => {
+                    setSortCallback(() =>(a, b) => b.amount - a.amount);
+                  }}
                 >
                   <title>Descending</title>
                   <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
@@ -77,22 +111,27 @@ function ExpenseTable({ expenses, setexpenses, setExpense, setEditingRowId }) {
           </tr>
         </thead>
         <tbody>
-          {filterData.map(({ id, title, category, amount }) => {
-            return (
-              <tr
-                key={id}
-                onContextMenu={(e) => {
-                  e.preventDefault();
-                  setMenuPosition({ left: e.clientX + 4, top: e.clientY + 4 });
-                  setRowId(id);
-                }}
-              >
-                <td>{title}</td>
-                <td>{category}</td>
-                <td>{amount}</td>
-              </tr>
-            );
-          })}
+          {filterData
+            .sort(sortCallback)
+            .map(({ id, title, category, amount }) => {
+              return (
+                <tr
+                  key={id}
+                  onContextMenu={(e) => {
+                    e.preventDefault();
+                    setMenuPosition({
+                      left: e.clientX + 4,
+                      top: e.clientY + 4,
+                    });
+                    setRowId(id);
+                  }}
+                >
+                  <td>{title}</td>
+                  <td>{category}</td>
+                  <td>{amount}</td>
+                </tr>
+              );
+            })}
 
           <tr>
             <th>Total</th>
